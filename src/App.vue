9ca2caf5520a11bb77123bb4c80c34bb3be84337
Leonardo <leonardodimatteo94@gmail.com>
2020-10-23 17:58:21 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <Navbar/>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator"
+import Navbar from "./components/Navbar.vue";
+
+@Component({
+  components: {
+    Navbar
+  }
+})
+export default class App extends Vue {
+}
+</script>
+<style>
+.router-link-active {
+  color: white !important;
+  text-decoration: none !important;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar dark fixed>
+    <router-link to="/">
+    <v-toolbar-title>
+      <v-avatar><v-img src="../assets/logo.png"></v-img></v-avatar>
+      Laze
+    </v-toolbar-title>
+    </router-link>
+  </v-app-bar>
+</template>
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class Navbar extends Vue {}
+</script>
